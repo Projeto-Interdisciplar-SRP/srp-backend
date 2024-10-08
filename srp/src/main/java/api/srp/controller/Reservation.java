7ef9d7721bdc2fe18d90c5393bd033cd99d6f7e3
Windows <Windows@DESKTOP-VVHQ7K1.mshome.net>
2024-10-08 11:11:18 -0300

    reserva

--- srp/src/main/java/api/srp/controller/Reservation.java
+++ srp/src/main/java/api/srp/controller/Reservation.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.srp.dto.response.ReservationResponseDTO;
+
 @RestController
 @RequestMapping("/reservation")
 public class Reservation {
 
 	@PostMapping(value={"/register", "register"})
-	public 
+	public ReservationResponseDTO register() {
+		
+		
+		
+	}
 	
 }
